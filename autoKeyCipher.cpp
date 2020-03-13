ddff03e919c289f27dc9a00e7927041fd78ce0c5
utkryuk <utkryuk@gmail.com>
2020-03-14 01:06:23 +0530

    Added AutoKey Cipher

--- autoKeyCipher.cpp
+++ autoKeyCipher.cpp
@@ -4,8 +4,12 @@ using namespace std;
 string encode(string pt,int k){
 	string ct = "";
 	for(int i=0;i<pt.length();i++){
-		if(pt[i]!=' ')
-			ct+= 'a'+ (((int)pt[i]-97)+k)%26;
+		if(pt[i]!=' '){
+			if(i==0)
+				ct+='a'+(((int)pt[i]-97)+k)%26;
+			else
+				ct+= 'a'+ (((int)pt[i]-97)+(int)ct[i-1]-97)%26;
+		}
 		else
 			ct+=pt[i];
 	}
@@ -14,8 +18,12 @@ string encode(string pt,int k){
 string decode(string ct,int k){
 	string chk = "";
 	for(int i=0;i<ct.length();i++){
-		if(ct[i]!=' ')
-			chk+='a' + (((int)ct[i]-97)-k+26)%26;
+		if(ct[i]!=' '){
+			if(i==0)
+				chk+='a' + (((int)ct[i]-97)-k+26)%26;
+			else
+				chk+='a' + (((int)ct[i]-97)-((int)ct[i-1]-97)+26)%26;
+		}
 		else
 			chk+=ct[i];
 	}
@@ -52,9 +60,9 @@ int main(){
 	string chk = decode(ct,k);
 	cout<<"Decoded Cipher Text: "<<chk<<"\n";
 	if(check(pt,chk))
-		cout<<"Additive Cipher done correctly\n";
+		cout<<"AutoKey Cipher done correctly\n";
 	else
-		cout<<"Additive Cipher done wrongly\n";
+		cout<<"AutoKey Cipher done wrongly\n";
 
 	return 0;
 }
